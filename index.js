076e6b5991387da69b1d9227e40373fffd8376f4
Rai Pritesh <84077582+mrjayrai@users.noreply.github.com>
2023-11-10 16:17:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
-const express = require("express");
-
-
-// const db = require('./db');
-
 
 const io = require("socket.io")(server, {
 	cors: {
@@ -15,16 +10,12 @@ const io = require("socket.io")(server, {
 });
 
 app.use(cors());
-app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
 	res.send('Running');
 });
-
-
-
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id);
